6a7a8ddfcbafd28687a76db05fe3ba4fcc3caee4
JosephAV04 <jbarreiro.jose@gmail.com>
2026-01-02 11:11:29 -0400

    Detalles Modal - Trabajo

--- Proyecto3-Calorias/my-app/src/components/AddFoodModal/AddFoodModal.tsx
+++ Proyecto3-Calorias/my-app/src/components/AddFoodModal/AddFoodModal.tsx
@@ -1,6 +1,7 @@
-import { Button, Icon } from '@rneui/themed';
-import React, { FC } from 'react'
-import {Modal, View, StyleSheet} from 'react-native'
+//import {Input  } from '@rneui/base';
+import { Button, Icon , Input } from '@rneui/themed';
+import React, { FC, useEffect, useState } from 'react'
+import {Modal, View, StyleSheet, Text} from 'react-native'
 
 type AddFoodModalProps = {
     onClose: () => void;
@@ -8,11 +9,60 @@ type AddFoodModalProps = {
 }
 
 const AddFoodModal: FC<AddFoodModalProps> = ({onClose, visible}) => {
+    const [calories, setCalories] = useState<string>('');
+    const [name, setName] = useState<string>('');
+    const [portion, setPortion] = useState<string>('');
+
+    useEffect(() => {
+        setCalories('');
+        setName('');
+        setPortion('');
+    }, [visible]);
+
+    const handleAddPress = () => {
+        onClose();
+    }
     return(
         <Modal visible = {visible} onRequestClose = {onClose} transparent animationType='slide'>
             <View style = {styles.container}>
                 <View style = {styles.content}>
-                    <Button icon={<Icon name='close' type="material-community" size={25}/> } onPress={onClose} type="clear" />
+                    <View style={styles.closeContainer}>
+                    <Button icon={<Icon name='close' type="material-community" size={28}/> } onPress={onClose} type="clear" />                    
+                    </View>
+                    <View style={styles.formItem}>
+                        <View style={styles.inputContainer}>
+                            <Input value ={calories} onChangeText={(text: string) => setCalories(text)} />
+                        </View>
+                        <View style={styles.legendContainer}>
+                            <Text style = {styles.legend}>CAL</Text>
+                        </View>
+                    </View>
+                    <View style={styles.formItem}>
+                        <View style={styles.inputContainer}>
+                            <Input value ={name} onChangeText={(text: string) => setName(text)} />
+                        </View>
+                        <View style={styles.legendContainer}>
+                            <Text style = {styles.legend}>Nombre</Text>
+                        </View>
+                    </View>
+                    <View style={styles.formItem}>
+                        <View style={styles.inputContainer}>
+                            <Input value ={portion} onChangeText={(text: string) => setPortion(text)} />
+                        </View>
+                        <View style={styles.legendContainer}>
+                            <Text style = {styles.legend}>Porcion</Text>
+                        </View>
+                    </View>
+                    <View style = {styles.buttonContainer}>
+                        <Button 
+                        title="Add" 
+                        icon={<Icon name="add" color="#fff"/>} 
+                        color="#4ecb71"
+                        radius="lg"
+                        disabled={calories.trim() === '' || name.trim() === '' || portion.trim() === ''}
+                        onPress={handleAddPress}
+                        />
+                    </View>
                 </View>
             </View>
         </Modal>
@@ -33,16 +83,39 @@ const styles = StyleSheet.create({
         width: '75%',
         backgroundColor: '#fff',
         padding: 18,
-        borderRadius: 24,
+        borderRadius: 20,
+
+        //Android shadow
         shadowColor: '#000',
         shadowOffset: {
             width: 0,
             height: 2,
         },
+        // iOs shadow
         shadowOpacity: 0.25,
         shadowRadius: 4,
         elevation: 5,
     },
+    closeContainer:{
+        alignItems:'flex-end',
+
+    },
+    formItem:{
+        flexDirection: 'row',
+        alignItems: 'center',
+    },
+    inputContainer:{
+        flex: 2,
+    },
+    legendContainer:{
+        flex: 1,
+    },
+    legend: {
+        fontWeight: 'bold',
+    },
+    buttonContainer:{
+        alignItems: 'flex-end',
+    }
 })
 export default AddFoodModal;
 
